c51e575a6be8dd5ce621451cb7ec01dae7932912
unknown <wanguilamya45@gmail.com>
2023-11-17 03:54:40 +0300

    first commit

---
+++ StudentDetails3.java
@@ -0,0 +1,11 @@
+public class StudentDetails {
+
+    public static void main(String[] args) {
+        // Printing information about the student
+        System.out.println("Name: LAMYA WANGUI");
+        System.out.println("Registration number: 22/03869");
+        System.out.println("Course: DIT");
+        System.out.println("Date of birth: 23/10/2002");
+        System.out.println("Status: Single");
+    }
+}
